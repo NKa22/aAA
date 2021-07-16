5af827f71a305dd86634e0d8921bed089d39a2a9
OGN <79502719+Jigara@users.noreply.github.com>
2021-07-15 22:11:11 -0400

    npc-mdt
    Initial files.

---
+++ fxmanifest.lua
@@ -0,0 +1,27 @@
+fx_version 'adamant'
+game 'gta5'
+
+description 'NPC-MDT'
+version '1.0.0'
+
+ui_page "ui/index.html"
+
+files {
+    "ui/index.html",
+    "ui/vue.min.js",
+    "ui/script.js",
+    "ui/main.css",
+    "ui/styles/police.css",
+    "ui/badges/police.png",
+    "ui/footer.png",
+    "ui/mugshot.png"
+}
+
+server_scripts {
+    '@mysql-async/lib/MySQL.lua',
+    "server/sv_mdt.lua",
+    "server/sv_vehcolors.lua"
+}
+
+client_script "client/cl_mdt.lua"
+client_script "@npc-scripts/client/errorlog.lua"
